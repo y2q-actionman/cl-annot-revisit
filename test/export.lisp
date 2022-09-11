f7f6defb644697f4a0ac1a60e4579773ef7f1667
y2q-actionman <y2q-actionman@users.noreply.github.com>
2022-09-11 22:47:23 +0900

    Added export tests

--- test/export.lisp
+++ test/export.lisp
@@ -1,40 +1,43 @@
 (in-package #:cl-annot-revisit-test)
 
-(test test-@export-single
-  (is (equal-after-macroexpand
-       '(cl-annot-revisit:export
-         #1=(defconstant hoge 100))
-       '(progn
-         (eval-when (:compile-toplevel :load-toplevel :execute) (export '(hoge)))
-         #1#)))
-  (is (equal-after-macroexpand
-       '(cl-annot-revisit:export
-         #2=(defun (setf func) (val) val))
-       '(progn
-         (eval-when (:compile-toplevel :load-toplevel :execute) (export '(func)))
-         #2#)))
-  (is (equal-after-macroexpand
-       '(cl-annot-revisit:export
-         #3=(defstruct struct1
-              slot1))
-       '(progn
-         (eval-when (:compile-toplevel :load-toplevel :execute) (export '(struct1)))
-         #3#)))
-  (is (equal-after-macroexpand
-       '(cl-annot-revisit:export
-         #4=(defstruct (struct2 (:copier cpstruct2))
-              slot1 slot2))
-       '(progn
-         (eval-when (:compile-toplevel :load-toplevel :execute) (export '(struct2)))
-         #4#))))
+(defun expanded-export-name-equalp (cl-annot-revisit-export-form name)
+  ;; (cl-annot-revisit:export (defconstant foo 100))
+  ;; ->
+  ;; (progn (eval-when (:compile-toplevel :load-toplevel :execute)
+  ;;          (export '(foo)))
+  ;;        (defconstant foo 100))
+  (equal-after-macroexpand
+   cl-annot-revisit-export-form
+   `(progn
+      (eval-when (:compile-toplevel :load-toplevel :execute)
+        (export '(,name)))
+      ,@(rest cl-annot-revisit-export-form))))
 
-(test test-@export-other
+(test test-export-single
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defconstant hoge 100))
+       'hoge))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defun (setf func) (val) val))
+       'func))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export
+         (defstruct struct1
+           slot1))
+       'struct1))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export
+         (defstruct (struct2 (:copier cpstruct2))
+           slot1 slot2))
+       'struct2)))
+
+(test test-export-other
   (is (equal-after-macroexpand
        '(cl-annot-revisit:export
          #1=(format t "Hello, World!"))
        '#1#)))
 
-(test test-@export-many
+(test test-export-many
   (is (equal-after-macroexpand
        '(cl-annot-revisit:export
          #1=(defconstant hoge 100)
@@ -49,3 +52,73 @@
        '(progn
          (cl-annot-revisit:export #3#)
          (cl-annot-revisit:export #4#)))))
+
+
+(test test-export-def-forms
+  ;; variables
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defconstant +defconstant-name+ 0))
+       '+defconstant-name+))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defparameter *defparameter-name* 1))
+       '*defparameter-name*))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defvar *defvar-name*))
+       '*defvar-name*))
+  ;; functions
+  ;; TODO: setf function, defgeneric, define-compiler-macro, defmethod, defun
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defgeneric defgeneric-name ()))
+       'defgeneric-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (define-compiler-macro define-compiler-macro-name ()))
+       'define-compiler-macro-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defmethod defmethod-name :around ()))
+       'defmethod-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defun defun-name ()))
+       'defun-name))
+  ;; others
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defclass defclass-name () ()))
+       'defclass-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (define-condition define-condition-name () ()))
+       'define-condition-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (define-method-combination define-method-combination-name))
+       'define-method-combination-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (define-modify-macro define-modify-macro-name () func))
+       'define-modify-macro-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (define-setf-expander define-setf-expander-name (var)))
+       'define-setf-expander-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (define-symbol-macro define-symbol-macro-name nil))
+       'define-symbol-macro-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defmacro defmacro-name ()))
+       'defmacro-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (defsetf defsetf-name foo))
+       'defsetf-name))
+  (is (expanded-export-name-equalp
+       '(cl-annot-revisit:export (deftype deftype-name ()))
+       'deftype-name))
+  ;; defstruct and defpackage is handled specially
+  )
+
+(test test-export-defpackage-form
+  (signals at-macro-style-warning
+    (let ((*at-macro-verbose* t))
+      (macroexpand
+       '(cl-annot-revisit:export
+         (defpackage defpackage-name)))))
+  (is
+   (let ((*at-macro-verbose* nil))
+     (equal-after-macroexpand
+      '(cl-annot-revisit:export
+        (defpackage defpackage-name))
+      '(defpackage defpackage-name)))))
