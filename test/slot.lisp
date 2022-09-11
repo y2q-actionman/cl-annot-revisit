9e15f0a935366b94e44bfd09a9828ae8bb1cc007
y2q-actionman <y2q-actionman@users.noreply.github.com>
2022-09-12 04:43:26 +0900

    Added test-optional-slots

--- test/slot.lisp
+++ test/slot.lisp
@@ -1,13 +1,21 @@
 (in-package :cl-annot-revisit-test)
 
-(test test-@optional
-  (is (equal (macroexpand-1 '(cl-annot-revisit:optional some-form slot1))
-             ''(slot1 :initform some-form :initarg :slot1)))
-  (is (equal (macroexpand '(cl-annot-revisit:optional #1=(+ foo bar) (slot2)))
-             ''(slot2 :initform #1# :initarg :slot2)))
-  (is (equal (macroexpand '(cl-annot-revisit:optional this-is-ignored
-                            (slot3 :initarg :argxxx :initform #2=(progn 1 2 3))))
-             ''(slot3 :initarg :argxxx :initform #2#)))
+(test test-optional-slots
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:optional some-form
+         slot1)
+       ''(slot1 :initform some-form :initarg :slot1)))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:optional #1=(+ foo bar)
+         (slot2))
+       ''(slot2 :initform #1# :initarg :slot2)))
+  (is (equal-after-macroexpand
+       #3='(cl-annot-revisit:optional this-is-ignored
+            (slot3 :initarg :argxxx :initform #2=(progn 1 2 3)))
+       ''(slot3 :initarg :argxxx :initform #2#)))
+  (signals at-macro-style-warning
+    (let ((*at-macro-verbose* t))
+      (macroexpand #3#)))
   (is (equal '(defclass foo ()
                (slot0
                 #.(cl-annot-revisit:optional initform1 slot1)
