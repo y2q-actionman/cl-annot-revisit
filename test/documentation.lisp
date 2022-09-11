f6a75fe4522b3a4c23bf3b788128559e31c5b4f4
y2q-actionman <y2q-actionman@users.noreply.github.com>
2022-09-11 20:21:10 +0900

    Cleanup doc tests

--- test/documentation.lisp
+++ test/documentation.lisp
@@ -25,46 +25,54 @@
          #:obj))))
 
 (test test-@doc-defclass
-  (is (equal-ignoring-gensym
-       (macroexpand-1 '(cl-annot-revisit:documentation #1="hoge"
-                        #2=(defclass x () ())))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation #1="hoge"
+         #2=(defclass x () ()))
        '(let ((#:obj #2#))
          (setf (documentation #:obj 'type) #1#)
          #:obj) ))
-  (is (equal-ignoring-gensym
-       (macroexpand-1 '(cl-annot-revisit:documentation #3="fuga"
-                        #4=(defclass y (super1) (slot1 slot2) (:metaclass meta))))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation #3="fuga"
+         #4=(defclass y (super1) (slot1 slot2) (:metaclass meta)))
        '(let ((#:obj #4#))
          (setf (documentation #:obj 'type) #3#)
          #:obj))))
 
 (test test-@doc-lambda
   ;; This is checking `parse-body' usage.
-  (is (equal
-       (macroexpand-1 '(cl-annot-revisit:documentation "doc" (lambda (x y z) (+ x y z))))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation "doc"
+         (lambda (x y z) (+ x y z)))
        '(lambda (x y z) "doc" (+ x y z))))
-  (is (equal
-       (macroexpand-1 '(cl-annot-revisit:documentation "doc" (lambda ()))) ; This is a bug of cl-annot.
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation "doc"
+         (lambda ())) ; This is a bug of cl-annot.
        '(lambda () "doc" nil)))
-  (is (equal
-       (macroexpand-1 '(cl-annot-revisit:documentation "doc" (lambda () nil)))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation "doc"
+         (lambda () nil))
        '(lambda () "doc" nil)))
-  (is (equal
-       (macroexpand-1 '(cl-annot-revisit:documentation "doc" (lambda () #1="this is not docstring")))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation "doc"
+         (lambda () #1="this is not docstring"))
        '(lambda () "doc" #1#)))
-  (is (equal
-       (macroexpand-1 '(cl-annot-revisit:documentation "doc" (lambda (x) (declare (ignore x)))))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation "doc"
+         (lambda (x) (declare (ignore x))))
        '(lambda (x) (declare (ignore x)) "doc" nil)))
-  (is (equal
-       (macroexpand-1 '(cl-annot-revisit:documentation "doc" (lambda (x) (declare (ignore x)) 100)))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation "doc"
+         (lambda (x) (declare (ignore x)) 100))
        '(lambda (x) (declare (ignore x)) "doc" 100)))
   t)
 
 (test test-@doc-lambda-func
-  (is (equal
-       (macroexpand-1 '(cl-annot-revisit:documentation "doc" #'(lambda (x y z) (+ x y z))))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation "doc"
+         #'(lambda (x y z) (+ x y z)))
        '#'(lambda (x y z) "doc" (+ x y z))))
-  (is (equal
-       (macroexpand-1 '(cl-annot-revisit:documentation "doc" #'(lambda ())))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:documentation "doc"
+         #'(lambda ()))
        '#'(lambda () "doc" nil)))
   t)
