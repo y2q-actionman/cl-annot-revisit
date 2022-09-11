6b6d3a6f7c7729d38e31dec0257a762af987d562
y2q-actionman <y2q-actionman@users.noreply.github.com>
2022-09-11 22:17:13 +0900

    Cleanup export tests

--- test/export.lisp
+++ test/export.lisp
@@ -1,61 +1,51 @@
 (in-package #:cl-annot-revisit-test)
 
 (test test-@export-single
-  (is (equal
-       (macroexpand
-        '(cl-annot-revisit:export
-          #1=(defconstant hoge 100)))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:export
+         #1=(defconstant hoge 100))
        '(progn
-         (eval-when #.+at-macro-eval-always+ (export '(hoge)))
+         (eval-when (:compile-toplevel :load-toplevel :execute) (export '(hoge)))
          #1#)))
-  (is (equal
-       (macroexpand
-        '(cl-annot-revisit:export
-          #2=(defun (setf func) (val) val)))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:export
+         #2=(defun (setf func) (val) val))
        '(progn
-         (eval-when #.+at-macro-eval-always+ (export '(func)))
+         (eval-when (:compile-toplevel :load-toplevel :execute) (export '(func)))
          #2#)))
-  (is (equal
-       (macroexpand
-        '(cl-annot-revisit:export
-          #3=(defstruct struct1
-               slot1)))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:export
+         #3=(defstruct struct1
+              slot1))
        '(progn
-         (eval-when #.+at-macro-eval-always+ (export '(struct1)))
+         (eval-when (:compile-toplevel :load-toplevel :execute) (export '(struct1)))
          #3#)))
-  (is (equal
-       (macroexpand
-        '(cl-annot-revisit:export
-          #4=(defstruct (struct2 (:copier cpstruct2))
-               slot1 slot2)))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:export
+         #4=(defstruct (struct2 (:copier cpstruct2))
+              slot1 slot2))
        '(progn
-         (eval-when #.+at-macro-eval-always+ (export '(struct2)))
-         #4#)))
-  t)
+         (eval-when (:compile-toplevel :load-toplevel :execute) (export '(struct2)))
+         #4#))))
 
 (test test-@export-other
-  (is (equal
-       (macroexpand
-        '(cl-annot-revisit:export
-          #1=(format t "Hello, World!")))
-       '#1#))
-  t)
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:export
+         #1=(format t "Hello, World!"))
+       '#1#)))
 
 (test test-@export-many
-  (is (equal
-       (macroexpand
-        '(cl-annot-revisit:export
-          #1=(defconstant hoge 100)
-          #2=(defconstant fuga 200)))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:export
+         #1=(defconstant hoge 100)
+         #2=(defconstant fuga 200))
        '(progn
          (cl-annot-revisit:export #1#)
          (cl-annot-revisit:export #2#))))
-  (is (equal
-       (macroexpand
-        '(cl-annot-revisit:export
-          #3=(defconstant hoge 100)
-          #4=(defun fuga () )))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:export
+         #3=(defconstant hoge 100)
+         #4=(defun fuga () ))
        '(progn
          (cl-annot-revisit:export #3#)
-         (cl-annot-revisit:export #4#))))
-  t)
+         (cl-annot-revisit:export #4#)))))
