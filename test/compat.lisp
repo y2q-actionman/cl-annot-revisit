08453dcad61ed2f1ee0f14d00f0a9875f71edf96
y2q-actionman <y2q-actionman@users.noreply.github.com>
2022-09-13 00:23:56 +0900

    Added compat test

--- test/compat.lisp
+++ test/compat.lisp
@@ -2,7 +2,7 @@
 
 (named-readtables:in-readtable cl-annot-revisit:at-syntax-readtable)
 
-(test test-compat-read-time-expansion
+(test test-compat-read-time-eval
   (let ((*cl-annot-compatibility* t))
     (is (equal
          (read-from-string
@@ -13,21 +13,42 @@
            (declare (ignorable x))
            (1+ x))))))
 
-#|
-CL-ANNOT-REVISIT/AT-SYNTAX> (count-macro-lambda-list-required-arguments '())
-0
-CL-ANNOT-REVISIT/AT-SYNTAX> (count-macro-lambda-list-required-arguments '(&whole a))
-0
-CL-ANNOT-REVISIT/AT-SYNTAX> (count-macro-lambda-list-required-arguments '(&whole a x y z))
-3
-CL-ANNOT-REVISIT/AT-SYNTAX> (count-macro-lambda-list-required-arguments '(&whole a x y z &key))
-3
-|#
+(test test-compat-alias
+  (let ((*cl-annot-compatibility* t))
+    (is (equal
+         (read-from-string
+          "(defun fuga (x)
+              @optimize (speed safety)
+              (1+ x))")
+         '(defun fuga (x)
+           (declare (optimize (speed safety)))
+           (1+ x))))))
 
-;; (defannotation @hoge (a) (:alias @fuga)
-;;   `(let ((obj ,a))
-;;      (pprint obj)
-;;      obj))
+(cl-annot-revisit-compat:defannotation hoge-annot (a)
+    (:inline nil :alias nil)
+  `(let ((obj ,a))
+     (pprint obj)
+     obj))
 
-;; (@hoge 100)
-;; (@fuga 100)
+(cl-annot-revisit-compat:defannotation fuga-annot (a b c)
+    (:inline nil :alias piyo-annot)
+  `(cl-annot-revisit:eval-always ,a ,b ,c))
+
+(cl-annot-revisit-compat:defannotation read-time-eval-annot (a)
+    (:inline t)
+  `(format nil "~A is ~A" ',a ,a))
+
+(test test-compat-defannotation
+  (is (equal-after-macroexpand
+       '@hoge-annot #1=(lambda (x) (+ 1 2 3))
+       '(let ((obj #1#))
+         (pprint obj)
+         obj)))
+  (is (equal-after-macroexpand
+       '@fuga-annot 100 200 300
+       '(eval-when (:compile-toplevel :load-toplevel :execute)
+         100 200 300)))
+  (is (equal-after-macroexpand
+       '@read-time-eval-annot (+ 1 2 3)
+       (format nil "~A is ~A" '(+ 1 2 3) (+ 1 2 3)))))
+
