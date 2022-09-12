a1e04b9eb0d834112c993bfef2088b8b029c5af8
y2q-actionman <y2q-actionman@users.noreply.github.com>
2022-09-12 10:01:59 +0900

    Cosmetic change

--- test/eval-when.lisp
+++ test/eval-when.lisp
@@ -1,21 +1,21 @@
 (in-package #:cl-annot-revisit-test)
 
 (test test-eval-when-compile
-  (is (equal (macroexpand
-              '(cl-annot-revisit:eval-when-compile foo))
-             '(eval-when (:compile-toplevel) foo))))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:eval-when-compile foo)
+       '(eval-when (:compile-toplevel) foo))))
 
 (test test-eval-when-load
-  (is (equal (macroexpand
-              '(cl-annot-revisit:eval-when-load bar))
-             '(eval-when (:load-toplevel) bar))))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:eval-when-load bar)
+       '(eval-when (:load-toplevel) bar))))
 
 (test test-eval-when-execute
-  (is (equal (macroexpand
-              '(cl-annot-revisit:eval-when-execute baz))
-             '(eval-when (:execute) baz))))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:eval-when-execute baz)
+       '(eval-when (:execute) baz))))
 
 (test test-eval-always
-  (is (equal (macroexpand
-              '(cl-annot-revisit:eval-always qux))
-             '(eval-when (:compile-toplevel :load-toplevel :execute) qux))))
+  (is (equal-after-macroexpand
+       '(cl-annot-revisit:eval-always qux)
+       '(eval-when (:compile-toplevel :load-toplevel :execute) qux))))
